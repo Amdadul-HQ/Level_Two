63a64d5a7013d88ebda64afd1b56323d860ade20
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-30 20:50:55 +0600

    adding object type optional types and literal types

---
+++ Module_1/src/ObjectOptional LiteralTypes.ts
@@ -0,0 +1,16 @@
+// Reference Type 
+
+const myInfo : {
+    firstName: string;
+    lastName: string;
+    age: number;
+    villageName?:string;// optional type
+    isSingle: boolean;
+    company: "Monster Studio"// literal type
+} = {
+    firstName: "Amdadul HQ",
+    lastName:"Rimon",
+    age:25,
+    isSingle: true,
+    company: "Monster Studio"
+}
