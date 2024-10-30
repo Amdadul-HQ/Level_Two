31757b968e764511b3e14f61211090424b8f77fc
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-30 20:25:11 +0600

    adding basic type

---
+++ Module_1/src/BasicDataType.ts
@@ -0,0 +1,36 @@
+// String
+const myName :string = "Amdadul Haque";
+
+// Number
+const age : number = 21
+
+// Boolean
+const isSingle : boolean = true;
+
+// undefined
+const x :undefined = undefined;
+
+// null
+const y : null = null;
+
+// any 
+let pani ;
+
+pani = 2
+
+pani ="2"
+
+pani = true
+
+
+// array of string
+const friends : string[] = ["Nabid Bhai","Ferdows Bhai", "Saif Bhai"]
+
+
+// array of number 
+const ages :number[] = [12,345,45]
+
+// tuple
+let [mrX,mrY] : [number,number] = [12,4]
+
+let info : [number,string] = [21,"Amdadul"]
