7faeab8af6afc402b5ada89621c078607c1e1f0c
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-31 15:11:00 +0600

    addin intersection

--- Module_1/src/ UnionandIntersectiontypes.ts
+++ Module_1/src/ UnionandIntersectiontypes.ts
@@ -2,25 +2,32 @@
 
     // UnionanIntersectiontypes
     type FrontEndDeveloper = "React.js" | "Anguler.js"
-    type FullStackDeveloper = "AWS" | "DevOps"
+    type BackDeveloper = "AWS" | "DevOps"
 
-    type Developer = FrontEndDeveloper | FullStackDeveloper;
+    type Developer = FrontEndDeveloper | BackDeveloper;
 
+    
+    
+    
     const fullStack :Developer = "AWS"
     
-
+    
     const newDeveloper :FrontEndDeveloper = "Anguler.js" 
-
+    
     type User = {
         name: string,
         blood: "A+" | "B+" | "O+",
         gender: "male" | "female"
     }
-
+    
     const user : User = {
         name:"Amdadul",
         blood:"A+",
         gender:"male"
     }
 
+    // Intersection
+    
+    type FullStackDeveloper  = FrontEndDeveloper & BackDeveloper;
+    
 }
