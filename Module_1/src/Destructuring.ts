fa8d0bd8dd4494a0f492f3c109ea171305d7962c
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-31 14:50:27 +0600

    adding array destructuring

---
+++ Module_1/src/Destructuring.ts
@@ -0,0 +1,21 @@
+const user = {
+    data:{
+        personalInfo:{
+            firstName:"Amdadul",
+            lastName:"Haque",
+            age:10
+        },
+        address:"Biroly",
+        phone:1236421
+    }
+}
+
+const {data:{personalInfo:{firstName}},data:{address:thikana}} = user
+
+// array destructuring
+
+const myFriend = ["Rimon","Saif","Nabid","Asif"]
+
+const [a,b,c,d] = myFriend
+
+
