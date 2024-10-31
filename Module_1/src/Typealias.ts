be35c98be073584607d783ea241ac4170abe180f
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-31 14:56:08 +0600

    adding type alias

---
+++ Module_1/src/Typealias.ts
@@ -0,0 +1,37 @@
+{
+// Type Alias
+
+type Student = {
+    name:string,
+    roll:number,
+    department:string,
+    phone:number
+}
+
+const student : {
+    name:string,
+    roll:number,
+    department:string,
+    phone:number
+} = {
+    name:"Amdadul",
+    roll:2,
+    department:"CT",
+    phone:223121
+}
+const student1 : Student = {
+    name:"Amdadu",
+    roll:1,
+    department:"CT",
+    phone:223121
+}
+
+console.log(student);
+console.log(student1);
+
+// function type alies
+
+
+
+
+}
