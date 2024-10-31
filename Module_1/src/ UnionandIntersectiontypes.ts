fb706cc1fbcba4518d6226b13836b89c63c8b722
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-10-31 15:04:12 +0600

    useing union type in type

---
+++ Module_1/src/ UnionandIntersectiontypes.ts
@@ -0,0 +1,20 @@
+{
+
+    // UnionanIntersectiontypes
+    type FrontEndDeveloper = "React.js" | "Anguler.js"
+
+    const newDeveloper :FrontEndDeveloper = "Anguler.js" 
+
+    type User = {
+        name: string,
+        blood: "A+" | "B+" | "O+",
+        gender: "male" | "female"
+    }
+
+    const user : User = {
+        name:"Amdadul",
+        blood:"A+",
+        gender:"male"
+    }
+
+}
