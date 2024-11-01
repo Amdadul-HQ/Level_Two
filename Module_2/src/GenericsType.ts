46bbd5339e7e0896d1a55223b7608573adaf064f
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-01 20:14:54 +0600

    adding generics type

---
+++ Module_2/src/GenericsType.ts
@@ -0,0 +1,15 @@
+{
+    // Generics Type
+    const rollNumber : number[] = [123,34535,456546]
+    // GenZ
+    const rollNumber1 : Array<number> = [1233,45565656,56]
+
+    const nameArray : string[] = ["Mr.x","Mr.y","Mr.y"];
+    // GenZ
+    const nameArray2 : Array<string> = ["adsfasd","asdfas","afdasd"]
+
+    const boolArray : boolean[] = [true,false,true]
+    // GenZ
+    const boolArray2 : Array<boolean> = [true,false,true]
+
+}
