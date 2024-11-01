14f593133b2d33afbfddeab27d9a467fa3685c0a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-01 20:22:09 +0600

    adding GenericType object with interface

--- Module_2/src/GenericsType.ts
+++ Module_2/src/GenericsType.ts
@@ -1,15 +1,42 @@
 {
+    type GenericType<T> = Array<T>
     // Generics Type
-    const rollNumber : number[] = [123,34535,456546]
+    const rollNumber : GenericType<number> = [123,34535,456546]
     // GenZ
-    const rollNumber1 : Array<number> = [1233,45565656,56]
+    const rollNumber1 : GenericType<number> = [1233,45565656,56]
 
-    const nameArray : string[] = ["Mr.x","Mr.y","Mr.y"];
+    const nameArray : GenericType<string> = ["Mr.x","Mr.y","Mr.y"];
     // GenZ
-    const nameArray2 : Array<string> = ["adsfasd","asdfas","afdasd"]
+    const nameArray2 : GenericType<string> = ["adsfasd","asdfas","afdasd"]
 
-    const boolArray : boolean[] = [true,false,true]
+    const boolArray :GenericType<boolean> = [true,false,true]
     // GenZ
-    const boolArray2 : Array<boolean> = [true,false,true]
+    const boolArray2 : GenericType<boolean> = [true,false,true]
+
+    interface User {
+        name:string;
+        age:number;
+        role?:string
+    }
+
+    const users : GenericType<User> = [
+        {
+            name: "Amdadul",
+            age:20
+        },
+        {
+            name: "Amdadul",
+            age:20
+        },
+        {
+            name: "Amdadul",
+            age:20
+        },
+        {
+            name: "Amdadul",
+            age:20
+        }
+    ]
+
 
 }
