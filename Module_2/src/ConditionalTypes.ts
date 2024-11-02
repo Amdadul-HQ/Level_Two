a3f3d39681972b252bde7b2ab7cb2e9106b8bdd8
AmdadulRimon <rimonamdadul301@gmail.com>
2024-11-02 16:40:43 +0600

    adding conditional type

--- Module_2/src/ConditionalTypes.ts
+++ Module_2/src/ConditionalTypes.ts
@@ -1,8 +1,22 @@
 {
     // Conditional Types
-    
+    type A1 = number
+    type B1 = null
+
+    type C1 = A1 extends null ? true : B1 extends string ? string : undefined
 
 
+    type Shaik = {
+        bike : string,
+        car : string,
+        ship: string
+    }
+
+    type CheckVehicle<T> = T extends keyof Shaik ?  true : false
+
+    type HasCar =  CheckVehicle<"car">
+
+    type HasTractor = CheckVehicle<"Tractor">
 
 
 }
