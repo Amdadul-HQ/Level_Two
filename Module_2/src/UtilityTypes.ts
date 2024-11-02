1f1f9c10093e95e055bf06dec6d505626c1b3208
AmdadulRimon <rimonamdadul301@gmail.com>
2024-11-02 17:20:21 +0600

    adding Required

--- Module_2/src/UtilityTypes.ts
+++ Module_2/src/UtilityTypes.ts
@@ -4,12 +4,23 @@
 type Person = {
     name: string,
     age:number,
-    email:string
+    email?:string,
+    contact:string
 }
 
 // Pick
 
 type All = Pick<Person,keyof Person>
 
+
+// Omit 
+type Contact = Omit<Person,"name" | "age">
+
+// Required
+
+type Require = Required<Person>
+
+
+// partial
 
 }
