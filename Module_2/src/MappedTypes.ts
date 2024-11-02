8f3daa82df15032e7ba01562358452809797ff22
AmdadulRimon <rimonamdadul301@gmail.com>
2024-11-02 16:59:05 +0600

    adding MappedType

---
+++ Module_2/src/MappedTypes.ts
@@ -0,0 +1,43 @@
+{
+// Mapped types
+ const arrayOfNumber = [1,2,45,67]
+
+//  const arrayOfString =["1","2","45","67"]
+
+const arrayOfString : string[] = arrayOfNumber.map((number)=>{
+    return number.toString();
+})
+
+type AreaType = {
+    height: number,
+    width: number,
+}
+
+// type AreaTypeString = {
+//     height: string,
+//     width:string
+// }
+
+const numberArray= [1,24,56,6]
+
+// interface NumberArray {
+//     [] : number
+// } 
+
+type AreaTypeString = {
+    [key in keyof AreaType] : string
+}
+
+type Height = AreaTypeString["height"]
+type Width = AreaType["width"]
+
+
+type AreaTypeString2<T> = {
+    [key in keyof T] : T[key]
+}
+
+
+
+
+
+}
