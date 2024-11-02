abde2325da890206236a9341dcec578371bd95c6
AmdadulRimon <rimonamdadul301@gmail.com>
2024-11-02 15:51:07 +0600

    adding asychornous data fetching

--- Module_2/src/AsynchronousTypescript.ts
+++ Module_2/src/AsynchronousTypescript.ts
@@ -1,7 +1,49 @@
 {
 
     // Asynchronous typescript
-    
+    const createPromise  = () : Promise<string> => {
+        const data :string = "Ase bhai ase"
+        return new Promise<string>((resolve,reject) => {
+            try{
+                if(data){
+                    resolve(data)
+                }
+                else{
+                    reject("Failed to fetch")
+                }
+            }
+            catch(error){
+                console.log(error);
+            }
+        })
+    }
+
+    const showData = async () =>{
+       const data :string = await createPromise()
+       return data
+    }
+
+    showData()
+
+    interface User {
+        userId:number,
+        id:number,
+        title: string,
+        completed: boolean
+    }
+
+    const getTodo = async (param:number) : Promise<User> => {
+        const url = `https://jsonplaceholder.typicode.com/todos/${param}`;
+
+        const response = await fetch(url)
+        const data = await response.json();
+        return await data
+    }
+
+const res = getTodo(5)
+console.log(res);
+
+
 
 
 
---
+++ Module_2/src/ConditionalTypes.ts
@@ -0,0 +1,8 @@
+{
+    // Conditional Types
+    
+
+
+
+
+}
