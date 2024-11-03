d81dc0f2ee2c5e32ca76221be2b3a8741af2731f
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-03 16:04:35 +0600

    adding task5

---
+++ Module_2/src/Task/Task5.ts
@@ -0,0 +1,26 @@
+{
+
+    // Task 5: Function Type
+    // Objective: Write a function that reverses a string.
+    
+    // Instructions:
+    
+    // Write a function reverseString that:
+    // Takes a single string argument.
+    // Returns the string in reverse order.
+    // Example:
+    // Input: "hello"
+    // Output: "olleh"
+
+    interface FunctionType {
+        (str : string) : string
+    }
+
+    const reverse :FunctionType = (str) =>  str.split("").reverse().join("")
+
+    console.log(reverse("rimon"));
+
+
+
+
+}
