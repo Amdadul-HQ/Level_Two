17fbeb71422671554cb8e288d544800dd1aabf57
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-03 20:15:29 +0600

    adding type guard using type of in

--- Module_3/src/TypeGuardUsingTypeof&In.ts
+++ Module_3/src/TypeGuardUsingTypeof&In.ts
@@ -16,7 +16,25 @@
     console.log(add(1,2));
     console.log(add("1","5"));
 
-    
+    type NormalUser = {
+        name:string
+    }
+    type  AdminUSer ={
+        name:string,
+        role: "admin"
+    }
+
+    const getUser = (param: NormalUser | AdminUSer) =>{
+        if("role" in param){
+            console.log(`My name is ${param.name}.I am a ${param.role}`);
+        }
+        else{
+            console.log(`My name is ${param.name}. I am a User`);
+        }
+    }
+
+    getUser({name:"Amdadul",role:"admin"})
+
 
 
 }
