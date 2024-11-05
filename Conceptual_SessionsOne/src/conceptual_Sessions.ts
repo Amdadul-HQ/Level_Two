ca9520f5d8bc93af6569e5ce29909cb6b2641eb7
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-05 18:41:14 +0600

    adding practice type

---
+++ Conceptual_SessionsOne/src/conceptual_Sessions.ts
@@ -0,0 +1,12 @@
+{
+
+// number Type
+const age:number = 25;
+// String Type
+const myName:string = "Rimon"
+// Boolean Type
+const isActive: boolean = true
+// Array string
+const numbers : number[] = [1,2,3,4,5]
+
+}
