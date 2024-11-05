b5cbc45c5dd952cb61e88c732f3db9175097379b
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-05 18:48:49 +0600

    adding Person type

--- Conceptual_SessionsOne/src/conceptual_Sessions.ts
+++ Conceptual_SessionsOne/src/conceptual_Sessions.ts
@@ -11,4 +11,23 @@ const numbers : number[] = [1,2,3,4,5]
 // Tuple
 const info : [string,boolean,number,object,[string,number]] = ["Rimon",true,2,{name:"rimon"},["rimon",231]]
 
+// object
+
+interface Person {
+    name:string,
+    age:number,
+    isActive:boolean,
+    designation:string,
+    company:string
+}
+
+const person : Person = {
+    name:"Amdadul Haque",
+    age:21,
+    isActive:true,
+    designation:"Next Level Developer",
+    company:"Programming hero"
+}
+
+
 }
