44ade104cdde81ad334d9962c3674dbcff323aef
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-05 18:44:29 +0600

    adding complex tuple

--- Conceptual_SessionsOne/src/conceptual_Sessions.ts
+++ Conceptual_SessionsOne/src/conceptual_Sessions.ts
@@ -8,5 +8,7 @@ const myName:string = "Rimon"
 const isActive: boolean = true
 // Array string
 const numbers : number[] = [1,2,3,4,5]
+// Tuple
+const info : [string,boolean,number,object,[string,number]] = ["Rimon",true,2,{name:"rimon"},["rimon",231]]
 
 }
